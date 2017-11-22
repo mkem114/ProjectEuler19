7c39c4063190bdde521f555a3ed4378c5a264e71
mkem114 <mkem114@aucklanduni.ac.nz>
2017-11-22 21:47:04 +1300

    Fixed bug by removing use of deprecated methods

--- src/ProjectEuler19.java
+++ src/ProjectEuler19.java
@@ -1,19 +1,20 @@
 import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.ZoneId;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.TimeZone;
 
 public class ProjectEuler19 {
 	public static void main(String[] args) {
 		new ProjectEuler19();
 	}
-//Something broke in Java as it gives off by two
+
+	//Something broke in Java as it gives off by two
 	ProjectEuler19() {
 		int count = 0;
-		Date dCount = new Date(1901, 1, 1);
-		LocalDate dateCount = dCount.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
-		Date dFinal = new Date(2000, 12, 31);
-		LocalDate dateFinal = dFinal.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+		LocalDate dateCount = LocalDate.of(1901, 1, 1);
+		LocalDate dateFinal = LocalDate.of(2000, 12, 31);
 		while (dateCount.isBefore(dateFinal)) {
 			if (dateCount.getDayOfWeek().equals(DayOfWeek.SUNDAY)) {
 				count++;
